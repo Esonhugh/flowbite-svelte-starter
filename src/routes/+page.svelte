c059cf23c5c8d3270e7ed5a20204a061f561b3ca
esonhugh <esonhughoutside@gmail.com>
2024-09-04 15:27:40 +0800

    update: side navigator bar feature.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<h1 class="text-3xl dark:text-white">Welcome to SvelteKit</h1>
-<p class="dark:text-white">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="dark:text-white p-2">
+	<h1 class="text-3xl">Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</div>
